af1f0ebfeff0942e2c9d3c827fe6ccb7192df889
Mike Oliver <remoteunderscore@gmail.com>
2022-12-12 17:51:30 +0000

    FizzBuzzUntil Code

---
+++ javascript-fundamentals/fizzbuzzUntil.js
@@ -0,0 +1,19 @@
+const fizzbuzzUntil = (number) => {
+  let i = 1
+
+  while (i <= number) {
+    if (i % 5 == 0 && i % 3 == 0) {
+      console.log ('fizzbuzz');
+    } else if (i % 3 == 0) {
+      console.log ('fizz');
+    } else if (i % 5 == 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+      i += 1
+  }
+}
+
+
+module.exports = fizzbuzzUntil;
